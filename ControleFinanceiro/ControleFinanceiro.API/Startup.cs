69e6678454b486bd57d1c1163f7194324b5ff8ce
Danilo Silva <danilo.silva@msn.com>
2021-10-06 21:24:27 -0300

    Criação do Contexto e Migrations

--- ControleFinanceiro/ControleFinanceiro.API/Startup.cs
+++ ControleFinanceiro/ControleFinanceiro.API/Startup.cs
@@ -1,5 +1,8 @@
+using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +22,10 @@ namespace ControleFinanceiro.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoBD")));
+
+            services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<Contexto>();
+
             services.AddCors();
 
             services.AddSpaStaticFiles(diretorio =>
